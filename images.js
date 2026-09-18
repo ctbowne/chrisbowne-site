fd4cd530275920bb4f3a76fa0166333916211744
Chris Bowne <63978936+ctbowne@users.noreply.github.com>
2026-09-17 20:07:09 -0700

    Add files via upload
    starting files/folders as created in Claude

---
+++ images.js
@@ -0,0 +1,11 @@
+// List every image you want on the site, in the order it should appear.
+// To add a new photo:
+//   1. Upload the image file into the "images" folder on GitHub.
+//   2. Add its filename to this list.
+// That's the whole process - no other code to touch.
+
+const IMAGES = [
+  "sample-1.svg",
+  "sample-2.svg",
+  "sample-3.svg"
+];
