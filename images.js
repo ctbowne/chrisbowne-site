a5188f59cf2949a2dc813d74054e341d651f6c8a
Chris Bowne <63978936+ctbowne@users.noreply.github.com>
2026-09-19 16:09:56 -0700

    Add files via upload
    updated files to include captions and expand on click capability

--- images.js
+++ images.js
@@ -1,11 +1,11 @@
 // List every image you want on the site, in the order it should appear.
 // To add a new photo:
 //   1. Upload the image file into the "images" folder on GitHub.
-//   2. Add its filename to this list.
-// That's the whole process - no other code to touch.
+//   2. Add an entry below with its filename and caption.
+// The "caption" text is optional - leave it as "" if you don't want one.
 
 const IMAGES = [
-  "sample-1.svg",
-  "sample-2.svg",
-  "sample-3.svg"
+  { file: "sample-1.svg", caption: "Replace me - sample 1" },
+  { file: "sample-2.svg", caption: "Replace me - sample 2" },
+  { file: "sample-3.svg", caption: "Replace me - sample 3" }
 ];
